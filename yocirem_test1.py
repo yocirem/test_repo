00b4d8e4d5e686f4ed24ad626c31505baef74f7d
yocirem <rem.cpe.mscs@gmail.com>
2019-08-02 14:30:47 +0800

    New Test Code

---
+++ yocirem_test1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+month = "August 2, 2019"
+
+print("Welcome to Python Data Science!")
+
+for char_letter in month:
+    print(char_letter)
